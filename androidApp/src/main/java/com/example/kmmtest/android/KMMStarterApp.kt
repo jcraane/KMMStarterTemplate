0e401a5daca7a8b21c04e5394099754b708aba3f
jamiecraane <jcraane@anwb.nl>
2022-09-19 22:13:51 +0200

    Configure logging based on BuildConfig

--- androidApp/src/main/java/com/example/kmmtest/android/KMMStarterApp.kt
+++ androidApp/src/main/java/com/example/kmmtest/android/KMMStarterApp.kt
@@ -1,24 +1,29 @@
 package com.example.kmmtest.android
 
 import android.app.Application
+import com.example.kmmstartertemplate.android.BuildConfig
 import com.example.kmmtest.di.HttpLoggingSpec
 import com.example.kmmtest.di.initKoin
 import com.example.kmmtest.localizationContext
 import io.ktor.client.plugins.logging.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
-import org.koin.dsl.module
 
 class KMMStarterApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
         localizationContext = this
-        initKoin(HttpLoggingSpec(true, LogLevel.ALL), appModule = appModule) {
-            androidLogger(Level.ERROR)
+        initKoin(createHttpLoggingSpec(), appModule = appModule) {
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidContext(this@KMMStarterApp)
         }
+    }
+
+    private fun createHttpLoggingSpec() = if (BuildConfig.DEBUG) {
+        HttpLoggingSpec(true, LogLevel.ALL)
+    } else {
+        HttpLoggingSpec(true, LogLevel.NONE)
     }
 }
