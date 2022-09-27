4fef58aa384925610fc9939a3f4bb8327feac6bd
jamiecraane <jcraane@anwb.nl>
2022-09-27 22:32:05 +0200

    Add const

--- shared/src/commonMain/kotlin/com/example/kmmtest/navigation/Routes.kt
+++ shared/src/commonMain/kotlin/com/example/kmmtest/navigation/Routes.kt
@@ -2,9 +2,9 @@ package com.example.kmmtest.navigation
 
 object Routes {
     object Profile {
-        private val basePath = "profile"
-        val ARG_USER_ID = "userId"
-        val route = "$basePath/{$ARG_USER_ID}"
+        private const val basePath = "profile"
+        const val ARG_USER_ID = "userId"
+        const val Addroute = "$basePath/{$ARG_USER_ID}"
         fun getNavigateRoute(userId: String) = "$basePath/$userId"
     }
 }
