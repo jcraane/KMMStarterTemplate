dbef55195e7c7adafcdc164fe05939aa6a3fcd33
jamiecraane <jcraane@anwb.nl>
2022-09-27 22:32:49 +0200

    Fix typo

--- shared/src/commonMain/kotlin/com/example/kmmtest/navigation/Routes.kt
+++ shared/src/commonMain/kotlin/com/example/kmmtest/navigation/Routes.kt
@@ -4,7 +4,7 @@ object Routes {
     object Profile {
         private const val basePath = "profile"
         const val ARG_USER_ID = "userId"
-        const val Addroute = "$basePath/{$ARG_USER_ID}"
+        const val route = "$basePath/{$ARG_USER_ID}"
         fun getNavigateRoute(userId: String) = "$basePath/$userId"
     }
 }
