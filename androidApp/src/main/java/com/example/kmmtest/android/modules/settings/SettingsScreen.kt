3065121686d6635525a9651fd77fb853326d5106
jamiecraane <jcraane@anwb.nl>
2022-09-27 09:23:05 +0200

    Add import for R

--- androidApp/src/main/java/com/example/kmmtest/android/modules/settings/SettingsScreen.kt
+++ androidApp/src/main/java/com/example/kmmtest/android/modules/settings/SettingsScreen.kt
@@ -6,15 +6,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.example.kmmstartertemplate.android.R
+import com.example.kmmstartertemplate.R.*
 
 @Composable
 fun SettingsScreen() {
     Column(
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Text(stringResource(com.example.kmmstartertemplate.R.string.l_bottomTab_settings_options_setting1))
-        Text(stringResource(com.example.kmmstartertemplate.R.string.l_bottomTab_settings_options_setting2))
-        Text(stringResource(com.example.kmmstartertemplate.R.string.l_bottomTab_settings_options_setting3))
+        Text(stringResource(string.l_bottomTab_settings_options_setting1))
+        Text(stringResource(string.l_bottomTab_settings_options_setting2))
+        Text(stringResource(string.l_bottomTab_settings_options_setting3))
     }
 }
