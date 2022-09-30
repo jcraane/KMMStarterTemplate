9e731edcad86bea13bd3074b4744de55fcec7810
jamiecraane <jcraane@anwb.nl>
2022-09-30 10:52:49 +0200

    Add images and code

---
+++ shared/src/commonMain/kotlin/com/example/kmmtest/f1/viewmodel/RaceDetails.kt
@@ -0,0 +1,9 @@
+package com.example.kmmtest.f1.viewmodel
+
+import com.example.kmmtest.Image
+
+data class RaceDetails(
+    val title: String,
+    val circuitName: String,
+    val circuit: Image,
+)
---
+++ shared/src/commonMain/kotlin/com/example/kmmtest/f1/viewmodel/RaceOverviewItem.kt
@@ -0,0 +1,8 @@
+package com.example.kmmtest.f1.viewmodel
+
+data class RaceOverviewItem(
+    val id: String,
+    val title: String,
+    val subTitle: String,
+    val caption: String,
+)
